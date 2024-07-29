caa2a4b77e66476a2fb9ac127091ab185ea56c8e
ChanoknanKhoonwat <chanoknan.kho@gmail.com>
2024-07-29 19:08:09 +0700

    first commit

---
+++ mpdf60/ttfontdata/barcode.mtx.php
@@ -0,0 +1,38 @@
+<?php
+$name='LibreBarcode39Text-Regular';
+$type='TTF';
+$desc=array (
+  'CapHeight' => 590,
+  'XHeight' => 400,
+  'FontBBox' => '[0 -400 450 600]',
+  'Flags' => 4,
+  'Ascent' => 600,
+  'Descent' => -400,
+  'Leading' => 0,
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 500,
+);
+$unitsPerEm=1000;
+$up=-75;
+$ut=50;
+$strp=240;
+$strs=50;
+$ttffile='C:/AppServ/www/SmartRMV2/mpdf60/ttfonts/LibreBarcode39Text-Regular.ttf';
+$TTCfontID='0';
+$originalsize=22924;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='barcode';
+$panose=' 0 0 0 0 0 0 0 0 0 0 0 0';
+$haskerninfo=false;
+$haskernGPOS=false;
+$hassmallcapsGSUB=false;
+$fontmetrics='win';
+// TypoAscender/TypoDescender/TypoLineGap = 600, -400, 0
+// usWinAscent/usWinDescent = 600, -400
+// hhea Ascent/Descent/LineGap = 600, -400, 0
+$useOTL=0x0000;
+$rtlPUAstr='';
+?>
